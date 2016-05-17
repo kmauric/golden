7df15ee113c7986444600c26f43b19abaa9ce95d
Kristine Mauric <kristine.mauric@gmail.com>
2016-05-16 21:03:02 -0400

    added responsive styles to midsized and large desktops

--- js/script.js
+++ js/script.js
@@ -1,41 +1,70 @@
 $(function() {
 
-//Smooth Scroll to sections from main-navigation menu
-$(".main-nav").find("a").each(function(){
-	var aLoc = $(this).attr("href");
-	$(this).on("click", function() {
-		$("html, body").animate({
-		scrollTop: $(aLoc).offset().top
-	}, 1500);
-	});
-}); //end smooth scroll
-
-// PORTFOLIO SECTION
-// Hover effect of images on the #portfolio section
-$(".portfolio-items").find("li").each(function(){
-	$(this).find("img")
-		.mouseover(function() {
-			$(this).fadeTo(500, 0.5);
-		})
-		.mouseout(function() {
-			$(this).fadeTo(500, 1);
+	//Smooth Scroll to sections from main-navigation menu
+	$(".main-nav").find("a").each(function(){
+		var aLoc = $(this).attr("href");
+		$(this).on("click", function() {
+			$("html, body").animate({
+			scrollTop: $(aLoc).offset().top
+		}, 1500);
 		});
-}); //end #portfolio image hover
+	}); //end smooth scroll
 
-$(".portfolio-items img").on("click", function(e) {
-	e.preventDefault();
-	var $modalImg = $("#myModal").find("img"),
-		$caption = $("#myModal").find("#caption");
-	$("body").css("overflow", "hidden");
-	$("#myModal").css("display", "block");
-	$modalImg.prop("src", $(this).prop("src"));
-	$caption.text($(this).prop("alt"));
-	$(window).scroll(function() { return false; });
-}); //end image modal
+	// PORTFOLIO SECTION
+	// Hover effect of images on the #portfolio section
+	$(".portfolio-items").find("li").each(function(){
+		$(this).find("img")
+			.mouseover(function() {
+				$(this).fadeTo(500, 0.5);
+			})
+			.mouseout(function() {
+				$(this).fadeTo(500, 1);
+			});
+	}); //end #portfolio image hover
+
+	$(".portfolio-items img").on("click", function(e) {
+		e.preventDefault();
+		var $modalImg = $("#myModal").find("img"),
+			$caption = $("#myModal").find("#caption");
+		$("body").css("overflow", "hidden");
+		$("#myModal").css("display", "block");
+		$modalImg.prop("src", $(this).prop("src"));
+		$caption.text($(this).prop("alt"));
+		$(window).scroll(function() { return false; });
+	}); //end image modal
+
+	$(".close").on("click", function(){
+		$("#myModal").css("display", "none");
+		$("body").css("overflow", "");
+	}); //end .close
+
 	
-$(".close").on("click", function(){
-	$("#myModal").css("display", "none");
-	$("body").css("overflow", "");
-}); //end .close
+	
+	// CONTACT US SECTION
+	
+	//Make placeholder text for inputs disapper on focus
+	$(".inputs").find("input[placeholder]").each(function() {
+		$(this).attr("data-placeholder", this.placeholder);
+		
+		$(this).bind("focus", function() {
+			this.placeholder = "";
+		});
+		
+		$(this).bind("blur", function() {
+			this.placeholder = $(this).attr("data-placeholder");
+		});
+	}); //end placeholder text disappear
+	
+	$(".textarea").find("textarea").each(function() {
+		$(this).attr("data-placeholder", this.placeholder);
+		
+		$(this).bind("focus", function() {
+			this.placeholder = "";
+		});
+		
+		$(this).bind("blur", function() {
+			this.placeholder = $(this).attr("data-placeholder");
+		});
+	}); //end textarea placeholder
 	
 }); //end jQuery function
